d1ff73419eca885cc7907b661aae8693e2778db7
git <tylerconner163@gmail.com>
2024-07-14 16:12:43 -0500

    back to my profile

--- tyler/src/Profile/Profile.js
+++ tyler/src/Profile/Profile.js
@@ -1,11 +1,11 @@
 import React from'react';
 import './Profile.css';
-import logo  from './d2.png';
+import logo  from './me2.png';
 
 function Profile(){
     return(
         <div className='profile'>
-            <h1>SexySummer14
+            <h1>Tyler
             </h1>
             <img src={logo} alt='fine' className='profile-pic'
            height={'600px'} width={'400px'} />
