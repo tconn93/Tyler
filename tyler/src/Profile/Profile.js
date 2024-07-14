5e27d3f438ac3e1a0216cf16ecc113816b0022c7
git <tylerconner163@gmail.com>
2024-07-14 16:44:58 -0500

    professional

--- tyler/src/Profile/Profile.js
+++ tyler/src/Profile/Profile.js
@@ -1,14 +1,12 @@
 import React from'react';
 import './Profile.css';
-import logo  from './me2.png';
+
 
 function Profile(){
     return(
         <div className='profile'>
-            <h1>Tyler
-            </h1>
-            <img src={logo} alt='fine' className='profile-pic'
-           height={'600px'} width={'400px'} />
+            <h1>Tyler Conner </h1>
+            <h4>Full Stack Software Engineer</h4>
         </div>
     )
 }
