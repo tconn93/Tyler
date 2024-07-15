71c8662990e979620a368355a4aafe7c02751722
git <tylerconner163@gmail.com>
2024-07-15 02:28:58 -0500

    aqua theme site

--- tyler/src/Sites/MobileSites.js
+++ tyler/src/Sites/MobileSites.js
@@ -4,6 +4,11 @@ function MobileSites(){
     return(
         <div className='mobile-site'>
             <h1>Mobile Sites</h1>
+            <ul>
+            <li><a href='https://pcolarugby.com'>Rugby</a></li>
+            <li><a href='https://tcon.app'>Private App</a></li>
+            <li><a href='https://sexysummer14.com'>Friend's Modeling Site</a></li>
+        </ul>
         </div>
     )
 }
--- tyler/src/Sites/Sites.js
+++ tyler/src/Sites/Sites.js
@@ -3,6 +3,11 @@ import React from'react';
 function Sites(){
     return <div>
         <h1>Sites</h1>
+        <ul>
+            <li><a href='https://pcolarugby.com'>Rugby</a></li>
+            <li><a href='https://tcon.app'>Private App</a></li>
+            <li><a href='https://sexysummer14.com'>Friend's Modeling Site</a></li>
+        </ul>
     </div>
 }
 
